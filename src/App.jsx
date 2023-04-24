1e179a101c1453d4da60211042a5ce4043ea792a
Firas <firasdabbabi@gmail.com>
2023-04-24 21:02:31 +0100

    version 1.5: touch App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { Switch, Route, Link } from 'react-router-dom';
 // import { Layout, Typography, Space } from 'antd';
 import { Navbar } from './components';
-
+import './App.css';
 
 export default function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="large" />
-                <Typography.Title level={2} className="logo">
+                <Typography.Title level={3} className="logo">
                     <Link to="/">BlackChain</Link>
                 </Typography.Title>
                 {/* <Button className='Menu-control-container'>
