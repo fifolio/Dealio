2291903fc09d39408e0ac917bdb5859f4bbbab67
Firas <firasdabbabi@gmail.com>
2023-04-26 18:00:52 +0100

    version 1.3: add Routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import './App.css';
 import { Navbar } from './components';
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
 
 
 export default function App() {
@@ -11,8 +16,17 @@ export default function App() {
         <Navbar />
       </div>
       <div className="main">
-
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className="footer">
 
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CryptoDetails() {
+    return (
+        <div>CryptoDetails</div>
+    )
+}
