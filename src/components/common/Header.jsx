71ec8343b85b527629623d0291036783477449d3
Firas <firasdabbabi@gmail.com>
2023-04-29 00:41:58 +0100

    verstion 1.3: update Header.jsx

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
+import { navlist } from "../../assets/data/data";
 import logo from "../assets/images/logo.png";
+import { BiSearch } from 'react-icons/bi'
+import { RiUser3Line } from 'react-icons/ri'
+import { AiOutlineHeart } from 'react-icons/ai'
+
+
 
 export default function Header() {
     return (
@@ -9,12 +16,33 @@ export default function Header() {
                         <div className="toggle">
                             <button>menu</button>
                         </div>
-                        <div className="left">
-                            <img src={logo} alt="logo" />
+                        <div className=" left">
+                            <img src={logo} alt="" />
+                        </div>
+                        <div className="center">
+                            <ul className="menu">
+                                {navlist.map((nav, index) => (
+                                    <li key={index}>
+                                        <Link to={nav.path}>
+                                            {nav.text}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </nav>
-                </div>
-            </header>
+                    <div className="right">
+                        <div className="right_search">
+                            <input type="text" placeholder="Search Products ..." />
+                            <BiSearch className="searchIcon heIcon" />
+                        </div>
+                        <div className="right_user">
+                            <RiUser3Line className='userIcon heIcon' />
+                            <AiOutlineHeart className='userIcon heIcon' />
+                        </div>
+                    </div>
+                </div >
+            </header >
         </>
     )
 }
