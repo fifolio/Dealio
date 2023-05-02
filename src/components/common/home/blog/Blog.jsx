94c42ca999be786a63f0cc37a5c40911ed350bdc
Firas <firasdabbabi@gmail.com>
2023-05-02 20:47:33 +0100

    version 2.7: add Blog.jsx with its posts from data.js

--- src/components/common/home/blog/Blog.jsx
+++ src/components/common/home/blog/Blog.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import Heading from '../../Heading';
+import { blog } from '../../../../assets/data/data'
 
 export default function Blog() {
     return (
-        <div>Blog</div>
+        <>
+            <section className="blog">
+                <Heading title="LATEST BLOG POSTS" desc="Latest marketplace news, success stories and tutorials" />
+
+                <div className="posts">
+                    {blog.slice(0, 3).map((items) => (
+                        <div className='post'>
+                            <div className="content">
+                                <div className="img">
+                                    <img src={items.cover} alt="" />
+                                </div>
+                                <div className="text">
+                                    <button className="button" style={{ marginBottom: '1px' }}>
+                                        {items.category}
+                                    </button>
+                                    <p>
+                                        Post Date : <span>{items.date}</span>
+                                    </p>
+                                    <h3>
+                                        {items.title.slice(0, 35)} ...
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </section>
+        </>
     )
 }
