6fd233a2097bc08632718de550d65587c855d889
Firas <firasdabbabi@gmail.com>
2023-05-02 17:44:35 +0100

    version 2.3: adding the products categories to TopProduct.jsx

--- src/components/common/home/topproduct/TopProduct.jsx
+++ src/components/common/home/topproduct/TopProduct.jsx
@@ -7,12 +7,23 @@ export default function TopProduct() {
 
     const [data, setData] = useState(topProducts)
 
+    const allCategories = ["all", ...new Set(data.map((item) => item.category))]
+
+    const [category, setCategory] = useState(allCategories)
+
     return (
         <>
             <section className="topproduct">
                 <div className="container">
                     <div className="head">
                         <Heading title="Top Selling Products" desc="Meet our newbies! The latest templates uploaded to the marketplace. " />
+                        <div className="category">
+                            {category.map((category) => (
+                                <button className="button">
+                                    {category}
+                                </button>
+                            ))}
+                        </div>
                     </div>
                     <ProductItem data={data} />
                 </div>
