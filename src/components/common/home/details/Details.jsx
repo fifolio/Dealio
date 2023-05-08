917a835e6621198f9872ee011e0ecccb16c5ee24
Firas <firasdabbabi@gmail.com>
2023-05-08 16:43:02 +0100

    version 4.2: mkdir & touch Details.jsx

---
+++ src/components/common/home/details/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Details() {
+    return (
+        <>
+            <article>
+                <section className='details'>
+                    <h2 className='details_title'>Product Details Pages</h2>
+                </section>
+            </article>
+        </>
+    )
+}
--- src/components/common/home/pages/Pages.jsx
+++ src/components/common/home/pages/Pages.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from '../../Header'
 import Footer from '../../Footer'
 import Home from '../Home'
+import Details from '../details/Details'
 
 
 export default function Pages() {
@@ -11,6 +12,7 @@ export default function Pages() {
             <Header />
             <Routes>
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/cart/:id" element={<Details />} />
             </Routes>
             <Footer />
         </>
